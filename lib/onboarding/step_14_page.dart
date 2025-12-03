e884a755736bd2f581c79ea571acfe5b4a0753d1
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-12-03 17:39:09 +0500

    imp 3

--- lib/onboarding/step_14_page.dart
+++ lib/onboarding/step_14_page.dart
@@ -8,8 +8,52 @@ class Step14Page extends StatefulWidget {
 }
 
 class _Step14PageState extends State<Step14Page> {
+  final options = [
+    'Travel ✈️',
+    'Cooking 🔍',
+    'Yoga 🧘',
+    'Hiking 🏔️',
+    'Movies 🎬',
+    'Pets 🐱',
+    'Photography 📷',
+    'Gaming 🎮',
+    'Music 🎵',
+    'Painting 🎨',
+    'Art 🎨',
+    'Sports 🏀',
+    'Fitness 💪',
+    'Reading 📚',
+    'Technology 📱',
+    'Dancing 🍁',
+    'Board Games 🎲',
+    'Science 🔬',
+    'Fashion 👗',
+    'Gardening 🌱',
+    'Coffee ☕',
+    'Astronomy 🌟',
+  ];
+  final selected = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            children: [
+              // Status Bar (ortga qaytish tugmasi, liner progress indicator, 9/12)
+
+              // Image, (Awesome! What topics do you like to talk about?)
+              Expanded(
+                child: Wrap(
+                  children: options.map((value) => Container(child: Text(value))).toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
