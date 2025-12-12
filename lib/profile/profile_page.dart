6d7a3d8f6e99c1db898d771f008ca9a362318fd3
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-12-12 17:46:37 +0500

    imp 14

--- lib/profile/profile_page.dart
+++ lib/profile/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,6 +10,54 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  List<String> titles = [
+    'Preferences',
+    'Manage Courses',
+    'Notification',
+    'Account & Security',
+    'Payment Methods',
+    'Billing & Subscriptions',
+    'Linked Accounts',
+    'App Appearance',
+    'Data & Analytics',
+    'Help & Support',
+    'Invite Your Friends',
+    'Rate us',
+    'Logout',
+  ];
+
+  List<Widget> icons = [
+    Icon(Icons.settings_outlined),
+    Icon(Icons.dashboard_outlined),
+    Icon(Icons.notifications_outlined),
+    Icon(Icons.security_outlined),
+    Icon(Icons.credit_card_outlined),
+    Icon(Icons.star_border),
+    Icon(Icons.link),
+    Icon(Icons.remove_red_eye_outlined),
+    Icon(Icons.bar_chart_outlined),
+    Icon(Icons.help_outline),
+    Icon(Icons.person_add_outlined),
+    Icon(Icons.star_outline),
+    Icon(Icons.logout, color: Colors.red),
+  ];
+
+  List<Widget> pages = [
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +65,29 @@ class _ProfilePageState extends State<ProfilePage> {
         child: Padding(
           padding: EdgeInsets.all(16),
           child: Column(
-            children: [],
+            children: [
+              Container(),
+              Container(),
+              Flexible(
+                fit: FlexFit.loose,
+                child: Container(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 13,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        onTap: () {
+                          Get.to(pages[index]);
+                        },
+                        leading: icons[index],
+                        title: Text(titles[index]),
+                        trailing: Icon(Icons.chevron_right),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
