66fb9edbe2ff655a1ee931afea1d8812ca90fedc
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-12-22 18:33:39 +0500

    imp 34

--- lib/word_cards/quiz_page.dart
+++ lib/word_cards/quiz_page.dart
@@ -8,15 +8,73 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<String> images = [
+    "assets/mariquita.png",
+    "assets/pulgones.png",
+  ];
+  List<String> variants = [
+    "Mariposa",
+    "Mariquita",
+    "Libélula",
+    "Pulgones",
+  ];
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xffF5F5F5),
+      appBar: AppBar(
+        backgroundColor: Color(0xffF5F5F5),
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(16),
           child: Column(
-            children: [],
+            children: [
+              SizedBox(height: 30),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Image.asset(images[currentIndex]),
+                ),
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                height: 200,
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 12,
+                    mainAxisSpacing: 12,
+                    childAspectRatio: 183 / 73,
+                  ),
+                  itemCount: 4,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        bool correct;
+                        if (index == 1) {
+                          correct = true;
+                        } else {
+                          correct = false;
+                        }
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Center(child: Text(variants[index])),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
