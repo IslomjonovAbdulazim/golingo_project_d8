95d59ccad90edd9c6782f1460450b37d4213659c
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-12-22 17:21:42 +0500

    imp 29

--- lib/word_cards/flashcard_page.dart
+++ lib/word_cards/flashcard_page.dart
@@ -11,12 +11,30 @@ class _FlashcardPageState extends State<FlashcardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xffF5F5F5),
+      appBar: AppBar(
+        backgroundColor: Color(0xffF5F5F5),
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(16),
           child: Column(
-            children: [],
+            children: [
+              SizedBox(height: 48),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+              SizedBox(height: 48),
+              Text("Tap to flip the card"),
+              SizedBox(height: 48),
+              Row(children: []),
+              SizedBox(height: 12),
+            ],
           ),
         ),
       ),
